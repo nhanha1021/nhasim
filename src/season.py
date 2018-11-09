fc3bbabb2b1e3e44cfba5da70dd12478e40d673a
Unknown <nha102196@gmail.com>
2018-11-09 18:06:14 -0500

    Season updates
    Main screen now prints out standings and updated weekly games with results.

--- src/season.py
+++ src/season.py
@@ -1,52 +1,72 @@
-import rostertool
-import random
+import rostertool, schedule, random
 from models import League
 from game import Game
 from tabulate import tabulate
 from os import system
-
-league = rostertool.loadLeague("USFL")
-game_history = []
-
-def create_schedule(league):
- 	schedule = []
- 	for hteam in league.allTeams():
- 		other_teams = league.allTeams()
- 		other_teams.remove(hteam)
- 		for ateam in other_teams:
- 			schedule.append(Game(ateam,hteam))
-	return schedule
-
-schedule = create_schedule(league)
-for i in range(5):
-	random.shuffle(schedule)
-
-standings = {}
-for team in league.allTeams():
-	standings[team.teamName] = [0,0]
-
 
 def print_standings(standings):
 	table = []
 	for team, record in standings.items():
 		table.append([team,record[0],record[1]])
 	table.sort(key=lambda x:int(x[1]), reverse=True)
-	print tabulate(table,["Team","W","L"])	
+	rankings = {}
+	for i in range(len(table)):
+ 		rankings[table[i][0]] = i+1
+	print(tabulate(table,["Team","W","L"]))	
+	print("")
+	return rankings
 
-while(True):
+def print_upcoming_games(cur_week, week_no,rankings):
+	table = []
+	for game in cur_week:
+		hot = ""
+		if(week_no>3):
+			if((rankings[game.awayTeam.teamName]<6) and (rankings[game.homeTeam.teamName]<6)):
+				hot = "Big Game!"
+		table.append(["{}_{}".format(game.awayTeam.teamName,rankings[game.awayTeam.teamName]),"{}_{}".format(game.homeTeam.teamName,rankings[game.homeTeam.teamName]),hot])
+	print(tabulate(table,["Away","Home",""]))
+
+def play_week(cur_week):
+	week_results = []
+	for game in cur_week:
+		gr = game.playGame()
+		week_results.append(gr)
+	return week_results
+
+def print_week_results(week_result,rankings):
+	table = []
+	for gr in week_result:
+		table.append(["{}_{}".format(gr.atname,rankings[gr.atname]),gr.atscore,gr.htscore,"{}_{}".format(gr.htname,rankings[gr.htname])])
+	print(tabulate(table,["Away","","","Home"]))
+
+def update_standings(standings, week_result):
+	for gr in week_result:
+		standings[gr.winner()][0]+=1
+		standings[gr.loser()][1]+=1
+
+league = rostertool.loadLeague("USFL")
+game_results = []
+
+schedule = schedule.make_schedule(league.allTeams())
+
+standings = {}
+for team in league.allTeams():
+	standings[team.teamName] = [0,0]
+
+week = 0;
+while(week<len(schedule)):
+	system("clear")
+	cur_week = schedule[week]
+	rankings = print_standings(standings)
+	print("Week {}".format(week+1))
+	print_upcoming_games(cur_week,week,rankings)
+	game_results.append(play_week(cur_week))
+	week_result = game_results[week]
+	update_standings(standings, week_result)
+	wait = raw_input()
 	system("clear")
 	print_standings(standings)
-	print ""
-	print("Upcoming Games:")
-	for i in range(3):
-		print schedule[i].get_headline()
-	print ""
-	if(len(schedule)<=0):
-		break
-	game = schedule.pop(0)
-	gr = game.playGame()
-	standings[gr.winner()][0]+=1
-	standings[gr.loser()][1]+=1
-	game_history.append(gr)
-	if(raw_input()=="quit"):
-		break
+	print("Week {} Results".format(week+1))
+	print_week_results(week_result,rankings)
+	wait = raw_input()
+	week += 1
--- src/tester.py
+++ src/tester.py
@@ -2,14 +2,11 @@ from models import League
 import schedule, rostertool
 
 league = rostertool.loadLeague("USFL")
-s = schedule.make_schedule(league.allTeams())
+s = schedule.make_schedule([i for i in range(1,17)])
 
 i = 0
 for w in s:
-	i += 1
-	print("\nWeek {}".format(i))
-	for g in w:
-		print("{} @ {}".format(g[0].teamName,g[1].teamName))
+	print w
 
 
 
