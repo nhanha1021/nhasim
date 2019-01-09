15a7cffd261d10f764e407f88d2a2558edaf6f82
Unknown <nha102196@gmail.com>
2019-01-09 16:20:27 -0500

    Updates to RosterTool and JsonTool

--- src/rostertool.py
+++ src/rostertool.py
@@ -3,45 +3,19 @@ from models import Team
 from models import League
 import json, jsontool, randomname, random
 
-PLAYER_DATA_PATH = "/Users/johnshea/Repos/nhasim_python/player_data/"
-TEAM_DATA_PATH = "/Users/johnshea/Repos/nhasim_python/team_data/"
 LEAGUE_DATA_PATH = "/Users/johnshea/Repos/nhasim_python/data/"
 
-def writePlayer(player):
-	data = jsontool.PlayerToJson(player)
-	filename = formatName(player.get_full_name())
-	with open(PLAYER_DATA_PATH+filename,'w') as datafile:
-		json.dump(data, datafile)
-
-def loadPlayer(playerName):
-	filename = formatName(playerName)
-	with open(PLAYER_DATA_PATH+filename,'r') as datafile:
-		data = json.load(datafile)
-		return jsontool.JsonToPlayer(data)
-
-def writeTeam(team):
-	data = jsontool.TeamToJson(team)
-	filename = formatName(team.get_team_name())
-	with open(TEAM_DATA_PATH+filename,'w') as datafile:
-		json.dump(data, datafile)
-
-def loadTeam(teamName):
-	filename = formatName(teamName)
-	with open(TEAM_DATA_PATH+filename,'r') as datafile:
-		data = json.load(datafile)
-		return jsontool.JsonToTeam(data)
-
 def writeLeague(league):
-	data = jsontool.LeagueToJson(league)
-	filename = formatName(league.leagueName)
+	data = jsontool.league_to_json(league)
+	filename = _to_file_name(league.get_league_name())
 	with open(LEAGUE_DATA_PATH+filename,'w') as datafile:
 		json.dump(data, datafile)
 
 def loadLeague(leagueName):
-	filename = formatName(leagueName)
+	filename = _to_file_name(leagueName)
 	with open(LEAGUE_DATA_PATH+filename,'r') as datafile:
 		data = json.load(datafile)
-		return jsontool.JsonToLeague(data)
+		return jsontool.json_to_league(data)
 
-def formatName(name):
+def _to_file_name(name):
 	return name.replace(" ","")+".json"
